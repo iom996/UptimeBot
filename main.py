a1bb39e3affb31da097915e156c40d49272181d4
iom996 <metelskiyivantop@gmail.com>
2025-06-10 20:32:44 +0200

    some changes

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, fsm
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from time import sleep
 import asyncio
-import configparser
+from os import getenv
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
+TOKEN = getenv("token")
 
-config = configparser.ConfigParser()
-config.read("../../config.ini")
+class Form(StatesGroup):
 
-TOKEN = config["token"]["token"]
+    web = State()
+
+    interval = State()
 
 dp = Dispatcher()
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+  await message.answer("Hello i am UpTime bot to Uptime send /uptime")
+
+@dp.message(Command("uptime"))
+async def command(message: Message, state: FSMContext) -> None :
+ await state.set_state(Form.web)
+ await message.answer(
+
+        "What is your web for Uptime?",
+
+        reply_markup=ReplyKeyboardRemove(),
+
+    )
+ #buttons = [[KeyboardButton(text='Cancel input')]]
+ #keyboard = ReplyKeyboardMarkup(keyboard=buttons)
+ #await message.answer('ok', reply_markup=keyboard)
+
+async def main() -> None:
+  bot = Bot(token=TOKEN)
+
+  await dp.start_polling(bot)
+
+if __name__ == '__main__' :
+  asyncio.run(main())
